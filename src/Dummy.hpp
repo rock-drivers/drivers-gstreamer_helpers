328970fba9502ee8ddc774eaf5941678b3409967
Sylvain <sylvain.joyeux@tidewise.io>
2023-03-03 08:47:05 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace gstreamer_helpers
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace gstreamer_helpers
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <gstreamer_helpers/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    gstreamer_helpers::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
